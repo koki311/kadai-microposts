d02208aa23830583800a80863f51d64edada0269
koki.kikuchi <kikuchi.k311@gmail.com>
2022-07-14 10:05:15 +0000

    fixing user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app') 
 
 @section('content')
-    <div class="center jumbotron">
-        <dix class="text-center">
-            <h1>Wellcome to the Microposts</h1>
-             {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-        </dix>
-    </div>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <dix class="text-center">
+                <h1>Wellcome to the Microposts</h1>
+                 {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </dix>
+        </div>
+    @endif
 @endsection
